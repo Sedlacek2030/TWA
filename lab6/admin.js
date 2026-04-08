028111ef16c3d95aae39715e20e8458f13c14b01
Sedlacek2030 <sedlap12@jcu.cz>
2026-04-08 12:24:22 +0000

    lab6

---
+++ lab6/admin.js
@@ -0,0 +1,5 @@
+FIREBASE_PROJECT_ID=https://twaprojekt-6aab2-default-rtdb.europe-west1.firebasedatabase.app/
+FIREBASE_CLIENT_EMAIL=your_client_email
+FIREBASE_PRIVATE_KEY=your_private_key
+ADMIN_USER=admin
+ADMIN_PASS=admin123
